a160d34b278644c51b6770ab1916b089cad74148
Tunicata <tunicata@pop-os.localdomain>
2021-09-25 16:26:54 -0400

    update scanner

---
+++ CSC254/Project02/parser_2021/scan.c
@@ -0,0 +1,106 @@
+/* Simple ad-hoc scanner for the calculator language.
+   Dies on lexical errors.
+   Michael L. Scott, 2008-2021.
+*/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "ctype.h"
+
+#include "scan.h"
+
+char token_image[MAX_TOKEN_LEN];
+
+token scan() {
+    static int c = ' ';
+        /* next available char; extra (int) width accommodates EOF */
+    int i = 0;              /* index into token_image */
+
+    /* skip white space */
+    while (isspace(c)) {
+        c = getchar();
+    }
+    if (c == EOF)
+        return t_eof;
+    if (isalpha(c)) {
+        do {
+            token_image[i++] = c;
+            if (i >= MAX_TOKEN_LEN) {
+                fputs("max token length exceeded\n", stderr);
+                exit(1);
+            }
+            c = getchar();
+        } while (isalpha(c) || isdigit(c) || c == '_');
+        token_image[i] = '\0';
+        if (!strcmp(token_image, "read")) return t_read;
+        else if (!strcmp(token_image, "write")) return t_write;
+        else if (!strcmp(token_image, "if")) return t_if;
+        else if (!strcmp(token_image, "fi")) return t_fi;
+        else if (!strcmp(token_image, "do")) return t_do;
+        else if (!strcmp(token_image, "od")) return t_od;
+        else if (!strcmp(token_image, "check")) return t_check;
+        else return t_id;
+    }
+    else if (isdigit(c)) {
+        do {
+            token_image[i++] = c;
+            c = getchar();
+        } while (isdigit(c));
+        token_image[i] = '\0';
+        return t_literal;
+    } else switch (c) {
+        case ':':
+            if ((c = getchar()) != '=') {
+                fprintf(stderr,
+                        "expected '=' after ':', got '%c' (0x%x)\n",
+                        c, c);
+                exit(1);
+            } else {
+                c = getchar();
+                return t_gets;
+            }
+            break;
+        case '+': c = getchar(); return t_add;
+        case '-': c = getchar(); return t_sub;
+        case '*': c = getchar(); return t_mul;
+        case '/': c = getchar(); return t_div;
+        case '(': c = getchar(); return t_lparen;
+        case ')': c = getchar(); return t_rparen;
+        case '=':
+            if ((c = getchar()) != '=') {
+                fprintf(stderr,
+                        "expected '=' after '=', got '%c' (0x%x)\n",
+                        c, c);
+                exit(1);
+            } else {
+                c = getchar();
+                return t_eqeq;
+            }
+            break;
+        case '>':
+            if ((c = getchar()) == '=') {
+                c = getchar();
+                return t_gte;
+            } else {
+                c = getchar();
+                return t_gt;
+            }
+            break;
+        case '<':
+            if ((c = getchar()) == '=') {
+                c = getchar();
+                return t_ste;
+            } else if ((c = getchar()) == '>') {
+                c = getchar();
+                return t_neq;
+            } else {
+                c = getchar();
+                return t_st;
+            }
+            break;
+        default:
+            fprintf(stderr, "unexpected character '%c' (0x%x)\n", c, c);
+            exit(1);
+    }
+}
---
+++ CSC254/Project02/parser_2021/scan.h
@@ -0,0 +1,12 @@
+/* Definitions the scanner shares with the parser.
+   Michael L. Scott, 2008-2021.
+*/
+
+typedef enum {t_read, t_write, t_id, t_literal, t_gets, t_add,
+              t_sub, t_mul, t_div, t_lparen, t_rparen, t_eof,
+              t_if, t_fi, t_do, t_od, t_check, t_eqeq, t_neq, t_gt, t_st, t_gte, t_ste} token;
+
+#define MAX_TOKEN_LEN 200
+extern char token_image[MAX_TOKEN_LEN];
+
+extern token scan();
